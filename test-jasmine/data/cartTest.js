817ad44a6c2dc97e35a29586f3ccb110c590687e
Divine Benjamin <divinebenjamin2004@gmail.com>
2026-01-02 21:21:30 +0100

    remove product tests

--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -11,6 +11,7 @@ describe('Test Suite: addToCart', () => {
         deliveryOptionId: '1'
       }]);
     });
+    loadFromStorage();
 
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
